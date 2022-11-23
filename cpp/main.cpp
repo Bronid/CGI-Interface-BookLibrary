a057baf79748ec27ce3b5fd7e630066af6363a43
Bronid <bronidgames@gmail.com>
2022-11-23 18:12:10 +0100

    Sqllite3 added, removed SqlLiteCPP

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-#include "../header/Database.h"
+#include <string>
 #include "sqlite3.h"
 
 using namespace std;
+
+char* err;
 
 static inline string getExamplePath()
 {
@@ -10,38 +12,40 @@ static inline string getExamplePath()
 	return filePath.substr(0, filePath.length() - std::string("main.cpp").length()) + "databases\\";
 }
 
+void AddDataToTable(sqlite3 *db) {
+	string book;
+	int year, pages;
+	cout << "Input book name:\n";
+	cin >> book;
+	cout << "Input year book:\n";
+	cin >> year;
+	cout << "Input pages:\n";
+	cin >> pages;
+	string query = "INSERT INTO Books VALUES('" + book + '\'' + "," + to_string(year) + "," + to_string(pages) + ");";
+	cout << query << "\n";
+	int res = sqlite3_exec(db, query.c_str(), NULL, NULL, &err);
+	if (res != SQLITE_OK) cout << "Insert error: " << err;
+}
+
 static const string filename_example_db3 = getExamplePath() + "example.db3";
 
 int main() {
+	int choose;
     sqlite3 *db;
     sqlite3_stmt* stmt;
     sqlite3_open("Test.db", &db);
-	cout << "Welcome to the CGI Book Library\nLibrary used in this project:\nSQLite3 version: " << SQLITECPP_VERSION;
-	const SQLite::Header header = SQLite::Database::getHeaderInfo(filename_example_db3);
-
-    std::cout << "Magic header string: " << header.headerStr << std::endl;
-    std::cout << "Page size bytes: " << header.pageSizeBytes << std::endl;
-    std::cout << "File format write version: " << (int)header.fileFormatWriteVersion << std::endl;
-    std::cout << "File format read version: " << (int)header.fileFormatReadVersion << std::endl;
-    std::cout << "Reserved space bytes: " << (int)header.reservedSpaceBytes << std::endl;
-    std::cout << "Max embedded payload fraction " << (int)header.maxEmbeddedPayloadFrac << std::endl;
-    std::cout << "Min embedded payload fraction: " << (int)header.minEmbeddedPayloadFrac << std::endl;
-    std::cout << "Leaf payload fraction: " << (int)header.leafPayloadFrac << std::endl;
-    std::cout << "File change counter: " << header.fileChangeCounter << std::endl;
-    std::cout << "Database size pages: " << header.databaseSizePages << std::endl;
-    std::cout << "First freelist trunk page: " << header.firstFreelistTrunkPage << std::endl;
-    std::cout << "Total freelist trunk pages: " << header.totalFreelistPages << std::endl;
-    std::cout << "Schema cookie: " << header.schemaCookie << std::endl;
-    std::cout << "Schema format number: " << header.schemaFormatNumber << std::endl;
-    std::cout << "Default page cache size bytes: " << header.defaultPageCacheSizeBytes << std::endl;
-    std::cout << "Largest B tree page number: " << header.largestBTreePageNumber << std::endl;
-    std::cout << "Database text encoding: " << header.databaseTextEncoding << std::endl;
-    std::cout << "User version: " << header.userVersion << std::endl;
-    std::cout << "Incremental vaccum mode: " << header.incrementalVaccumMode << std::endl;
-    std::cout << "Application ID: " << header.applicationId << std::endl;
-    std::cout << "Version valid for: " << header.versionValidFor << std::endl;
-    std::cout << "SQLite version: " << header.sqliteVersion << std::endl;
-
+	int res = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Books(BookName varchar(100), Year int, Pages int);", NULL, NULL, &err);
+	if (res != SQLITE_OK) cout << "Error: " << err;
+	cout << "Welcome to the CGI Book Library\nLibrary used in this project:\nSQLite3 version: " << sqlite3_version;
+	cout << "Choose:\n0. Exit\n1. Add new book\n";
+	cin >> choose;
+	switch(choose) {
+	case 0: return EXIT_SUCCESS;
+		break;
+	case 1: AddDataToTable(db);
+		break;
+	default: return EXIT_FAILURE;
+	}
 	//Database db(filename_example_db3);
 	//db.ListGreaterThan(5);
 }
